a5c339c624dedb037843ac1341094c5f97b98f3b
Ilia Stepanov <istepanov@ru.estee.com>
2023-11-25 17:59:35 +0100

    replacing internal cache with cosmos container

---
+++ src/main/java/com/chtrembl/petstore/order/configuration/CosmosDbConfiguration.java
@@ -0,0 +1,36 @@
+package com.chtrembl.petstore.order.configuration;
+
+
+import com.azure.cosmos.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class CosmosDbConfiguration {
+
+    @Value("${petstore.cosmos.account_host}")
+    private String accountHost;
+    @Value("${petstore.cosmos.account_key}")
+    private String accountKey;
+
+
+    @Bean
+    public CosmosClient cosmosClient() {
+        return new CosmosClientBuilder()
+                .endpoint(accountHost)
+                .key(accountKey)
+                .preferredRegions(Collections.singletonList("West US"))
+                .consistencyLevel(ConsistencyLevel.EVENTUAL)
+                .buildClient();
+    }
+
+    @Bean
+    public CosmosContainer ordersContainer(CosmosClient cosmosClient) {
+        CosmosDatabase petstore = cosmosClient.getDatabase("petstore");
+        return petstore.getContainer("orders");
+    }
+
+}
