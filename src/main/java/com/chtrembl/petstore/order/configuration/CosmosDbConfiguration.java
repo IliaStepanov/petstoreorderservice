ae3ee3fb6d8b2a2e35b93fad252eb5a393da8498
Ilia Stepanov <istepanov@ru.estee.com>
2023-12-18 11:55:25 +0100

    Revert "removing CosmosDbConfiguration"

--- src/main/java/com/chtrembl/petstore/order/configuration/CosmosDbConfiguration.java
+++ src/main/java/com/chtrembl/petstore/order/configuration/CosmosDbConfiguration.java
@@ -5,12 +5,10 @@ import com.azure.cosmos.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.util.Collections;
 
 @Configuration
-@Profile("final")
 public class CosmosDbConfiguration {
 
     @Value("${petstore.cosmos.account_host}")
